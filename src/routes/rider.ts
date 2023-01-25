3bdc50354f8e2ee5464141565d89b27ec9d0828d
Jayanth <jayanth151002@gmail.com>
2023-01-25 11:46:29 +0530

    Added routing.exe file

--- src/routes/rider.ts
+++ src/routes/rider.ts
@@ -6,7 +6,7 @@ import { authRider } from "../controllers/riders/authRider.controller";
 import { riderConsignments } from "../controllers/riders/consignments.controller";
 
 const riderRouter = Router();
-// riderRouter.get('/routing', runRoutingAlgo);
+riderRouter.get('/routing', runRoutingAlgo);
 riderRouter.get('/new', createNewRiders);
 riderRouter.post('/login', authRider);
 riderRouter.get('/consignments', verifyToken, riderConsignments);
