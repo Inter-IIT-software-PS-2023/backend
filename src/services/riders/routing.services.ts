51da15ace9e0d70bb71a2de4d6ae60823ca5736a
Jayanth <jayanth151002@gmail.com>
2023-01-25 22:17:52 +0530

    Changes in child process

--- src/services/riders/routing.services.ts
+++ src/services/riders/routing.services.ts
@@ -2,13 +2,15 @@ import { execFile, spawn } from "child_process"
 
 export const routingAlgo = async () => {
 
-    const child = spawn("./exe 300 10 12.971599 77.638725")
+    const child = spawn("./exe", ["300", "10", "12.971599", "77.638725"])
     child.stdin.end()
     const childProcessResp = new Promise((resolve, reject) => {
         child.stdout.on("data", (data) => {
+            console.log(data)
             resolve(data)
         })
         child.stderr.on("data", (data) => {
+            console.log(data)
             reject(data)
         })
     })
