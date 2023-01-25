00985acd44fce09e8bc032db84650edbeacc7e90
Jayanth <jayanth151002@gmail.com>
2023-01-25 22:46:15 +0530

    Changes in child process

--- src/services/riders/routing.services.ts
+++ src/services/riders/routing.services.ts
@@ -2,33 +2,33 @@ import { execFile, spawn } from "child_process"
 
 export const routingAlgo = async () => {
 
-    // const child = spawn("./exe", ["300", "10", "12.971599", "77.638725"])
-    // const childProcessResp = new Promise((resolve, reject) => {
-    //     child.stdout.on("data", (data) => {
-    //         console.log(data)
-    //         resolve(data)
-    //     })
-    //     child.stderr.on("data", (data) => {
-    //         console.log(data)
-    //         reject(data)
-    //     })
-    // })
-    // return await childProcessResp
-
+    const child = spawn("./exe", ["300", "10", "12.971599", "77.638725"])
     const childProcessResp = new Promise((resolve, reject) => {
-        execFile("./exe", ["300", "10", "12.971599", "77.638725"], (err, data) => {
-            if (err) {
-                console.log("\n\n\nError\n\n\n")
-                console.log(err)
-                reject(err)
-            }
-            else {
-                console.log("\n\n\Data\n\n\n")
-                resolve(data)
-            }
+        child.stdout.on("data", (data) => {
+            console.log(data)
+            resolve(data)
+        })
+        child.stderr.on("data", (data) => {
+            console.log(data)
+            reject(data)
         })
     })
     return await childProcessResp
+
+    // const childProcessResp = new Promise((resolve, reject) => {
+    //     execFile("./exe", ["300", "10", "12.971599", "77.638725"], (err, data) => {
+    //         if (err) {
+    //             console.log("\n\n\nError\n\n\n")
+    //             console.log(err)
+    //             reject(err)
+    //         }
+    //         else {
+    //             console.log("\n\n\Data\n\n\n")
+    //             resolve(data)
+    //         }
+    //     })
+    // })
+    // return await childProcessResp
 }
 
 
