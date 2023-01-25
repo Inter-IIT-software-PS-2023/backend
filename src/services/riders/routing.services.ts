b80081b1c209efc7713e27a731dc908160c73743
Jayanth <jayanth151002@gmail.com>
2023-01-25 23:01:58 +0530

    Changes in child process

--- src/services/riders/routing.services.ts
+++ src/services/riders/routing.services.ts
@@ -7,7 +7,7 @@ export const routingAlgo = async () => {
     const childProcessResp = new Promise((resolve, reject) => {
         child.stdout.on("data", (data) => {
             console.log(data)
-            resolve(data)
+            resolve(data.toString())
         })
         child.stderr.on("data", (data) => {
             console.log(data)
