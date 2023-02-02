aec4e9c254a67bf015961e68b464927d43d2f88b
Jayanth <ae20b030@smail.iitm.ac.in>
2023-02-02 18:14:04 +0530

    Added delivery lat and long in orders

--- src/controllers/riders/delivery.controller.ts
+++ src/controllers/riders/delivery.controller.ts
@@ -3,8 +3,8 @@ import { deliveryService } from "../../services/riders/delivery.service";
 
 export const deliveryController = async (req: Request, res: Response) => {
     try {
-        const { productId } = req.body.productId;
-        const order = await deliveryService(productId);
+        const { productId, lat, lng } = req.body;
+        const order = await deliveryService(productId, lat, lng);
         res.json({ msg: "Order delivered", data: order });
     }
     catch (err) {
--- src/services/riders/delivery.service.ts
+++ src/services/riders/delivery.service.ts
@@ -2,13 +2,15 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient()
 
-export const deliveryService = async (productId: string) => {
+export const deliveryService = async (productId: string, lat: string, lng: string) => {
     const updatedOrder = await prisma.order.update({
         where: {
             productId: productId
         },
         data: {
-            status: "DELIVERED"
+            status: "DELIVERED",
+            deliveryLat: parseFloat(lat),
+            deliveryLng: parseFloat(lng)
         }
     })
     return updatedOrder
