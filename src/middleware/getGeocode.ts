bc03d08624f30e534e0bda6e0970fb27f66fb0b5
Jayanth <ae20b030@smail.iitm.ac.in>
2023-02-04 16:07:23 +0530

    Thrashed Mapbox API, Google Geocoding API comes to the rescue

--- src/middleware/getGeocode.ts
+++ src/middleware/getGeocode.ts
@@ -1,13 +1,13 @@
 import dotenv from "dotenv"
 dotenv.config()
 
-const api_key = process.env.MAPBOX_API_KEY
+const api_key = process.env.GOOGLE_MAP_API_KEY
 
 export const getGeocode = async (address: string) => {
-    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${api_key}`
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(address)}&key=${api_key}`
     const info = fetch(url)
         .then((res) => res.json())
-        .then((data) => [data.features[0].center[0], data.features[0].center[1]] as [number, number])
+        .then((data) => [data.results[0].geometry.location.lat, data.results[0].geometry.location.lng] as [number, number])
         .catch((err) => new Error("Error in fetching geocodes"))
-    return info 
+    return info
 }
