d63c3a35213e227d42bf04f10fde0cae3a33a7f3
Jayanth <ae20b030@smail.iitm.ac.in>
2023-02-04 12:00:44 +0530

    Added delivery time calculation feature

--- src/services/riders/delivery.service.ts
+++ src/services/riders/delivery.service.ts
@@ -3,6 +3,11 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
 
 export const deliveryService = async (awb: string, lat: string, lng: string) => {
+    const date = new Date()
+    const deliveryTimeInMins = (date.getHours() - 3) * 60 + date.getMinutes()- 30 
+    // 5 hours and 30 mins is difference between IST and UTC time    
+    // We assume that the delivery process starts at 9 am IST
+    // So we subtract 3 hours 30 mins from the current time to get the time in UTC
     const updatedOrder = await prisma.order.update({
         where: {
             awb: awb
@@ -10,7 +15,8 @@ export const deliveryService = async (awb: string, lat: string, lng: string) => 
         data: {
             status: "DELIVERED",
             deliveryLat: parseFloat(lat),
-            deliveryLng: parseFloat(lng)
+            deliveryLng: parseFloat(lng),
+            deliveryTime: deliveryTimeInMins
         }
     })
     return updatedOrder
