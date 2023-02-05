500d6c6a6f0ea191a93cbeb5fadcd90dcc8edaeb
Jayanth <ae20b030@smail.iitm.ac.in>
2023-02-05 13:30:23 +0530

    Added firebase notification feature for dynamic addition of orders

---
+++ src/admin/config.ts
@@ -0,0 +1,8 @@
+import { initializeApp, ServiceAccount } from 'firebase-admin/app';
+import { credential } from 'firebase-admin';
+import serviceAccount from "./serviceAccountKey.json"
+
+export default initializeApp({
+    credential: credential.cert(serviceAccount as ServiceAccount),
+    databaseURL: "https://<DATABASE_NAME>.firebaseio.com"
+})
--- src/services/orders/addOrder.service.ts
+++ src/services/orders/addOrder.service.ts
@@ -1,3 +1,32 @@
-export const addOrderService = async (address: string) => {
+import { PrismaClient } from "@prisma/client"
+import { messaging } from "firebase-admin"
+import adminConfig from "../../admin/config"
+const prisma = new PrismaClient()
 
+export const addOrderService = async (address: string) => {
+    const riderIdFromAlgo = "1"
+    const rider = await prisma.rider.findUnique({
+        where: {
+            username: `dpartner_${riderIdFromAlgo}`
+        }
+    })
+    if (!rider) {
+        throw new Error("Cannot allot dynamic point")
+    }
+    else {
+        const fcmToken = rider.fcmToken
+        const message = {
+            notification: {
+                title: "New Order",
+                body: "You have a new order to deliver"
+            }
+        }
+        messaging(adminConfig).sendToDevice(fcmToken, message)
+        .then((response) => {
+            console.log("Successfully sent message:", response);
+        })
+        .catch((error) => {
+            console.log("Error sending message:", error);
+        })
+    }
 }
