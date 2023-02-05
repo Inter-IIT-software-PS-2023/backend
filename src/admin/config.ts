fc411613976df7b68ac838248214730037f47ad3
Jayanth <ae20b030@smail.iitm.ac.in>
2023-02-05 18:08:01 +0530

    Add orders service done

--- src/admin/config.ts
+++ src/admin/config.ts
@@ -1,8 +1,8 @@
-import { initializeApp, ServiceAccount } from 'firebase-admin/app';
-import { credential } from 'firebase-admin';
-import serviceAccount from "./serviceAccountKey.json"
+// import { initializeApp, ServiceAccount } from 'firebase-admin/app';
+// import { credential } from 'firebase-admin';
+// import serviceAccount from "./serviceAccountKey.json"
 
-export default initializeApp({
-    credential: credential.cert(serviceAccount as ServiceAccount),
-    databaseURL: "https://<DATABASE_NAME>.firebaseio.com"
-})
+// export default initializeApp({
+//     credential: credential.cert(serviceAccount as ServiceAccount),
+//     databaseURL: "https://<DATABASE_NAME>.firebaseio.com"
+// })
--- src/services/orders/addOrder.service.ts
+++ src/services/orders/addOrder.service.ts
@@ -1,32 +1,41 @@
 import { PrismaClient } from "@prisma/client"
 import { messaging } from "firebase-admin"
-import adminConfig from "../../admin/config"
+// import adminConfig from "../../admin/config"
+import { getGeocode } from "../../middleware/getGeocode"
 const prisma = new PrismaClient()
 
 export const addOrderService = async (address: string) => {
     const riderIdFromAlgo = "1"
-    const rider = await prisma.rider.findUnique({
-        where: {
-            username: `dpartner_${riderIdFromAlgo}`
-        }
-    })
-    if (!rider) {
-        throw new Error("Cannot allot dynamic point")
-    }
+    const newGeoCodes = await getGeocode(address)
+        .then(async coords => coords)
+        .catch(err => err)
+    if (newGeoCodes instanceof Error)
+        throw new Error("Error in fetching geocodes")
     else {
-        const fcmToken = rider.fcmToken
-        const message = {
-            notification: {
-                title: "New Order",
-                body: "You have a new order to deliver"
+        const rider = await prisma.rider.findUnique({
+            where: {
+                username: `dpartner_${riderIdFromAlgo}`
             }
+        })
+        if (!rider) {
+            throw new Error("Cannot allot dynamic point")
         }
-        messaging(adminConfig).sendToDevice(fcmToken, message)
-        .then((response) => {
-            console.log("Successfully sent message:", response);
-        })
-        .catch((error) => {
-            console.log("Error sending message:", error);
-        })
+        else {
+            // const fcmToken = rider.fcmToken
+            // const message = {
+            //     notification: {
+            //         title: "New Pickup Point",
+            //         body: "You have a new pickup point at " + address
+            //     }
+            // }
+            // messaging(adminConfig).sendToDevice(fcmToken, message)
+            //     .then((response) => {
+            //         console.log("Successfully sent message:", response);
+            //     })
+            //     .catch((error) => {
+            //         console.log("Error sending message:", error);
+            //     })
+            
+        }
     }
 }
