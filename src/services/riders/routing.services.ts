fbcc9b49a28aec54bd14da56208a014e79690b16
Jayanth <ae20b030@smail.iitm.ac.in>
2023-02-07 00:28:29 +0530

    Updated routing services

--- src/services/riders/routing.services.ts
+++ src/services/riders/routing.services.ts
@@ -1,4 +1,4 @@
-import {  exec } from "child_process"
+import { exec } from "child_process"
 import { Cluster, Order, PrismaClient } from "@prisma/client"
 import fs from "fs"
 import path from "path"
@@ -30,7 +30,7 @@ export const routingAlgo = async () => {
     const execPath = path.join(__dirname, "../../../src/algorithms/tsp.out")
     // const execPath = path.join(__dirname, "../../../src/algorithms/exe")
     const inputFilePath = path.join(__dirname, "../../../src/algorithms/routingInput.txt")
-    const outputFilePath = path.join(__dirname, "../../../src/algorithms/routingOutput.txt")
+    const outputFilePath = path.join(__dirname, "../../../src/algorithms/output.txt")
     algoInput = JSON.stringify(algoInput)
     algoInput.replace("\n", " ")
     const algoOutput = new Promise((resolve, reject) => {
@@ -39,63 +39,64 @@ export const routingAlgo = async () => {
                 reject({ err: err.message })
             }
             else {
-                exec(`"${execPath}" ${noOfHours} < "${inputFilePath}" > "${outputFilePath}"`, (err, stdout, stderr) => {
-                    if (err) {
-                        console.log("err ", err)
+                // exec(`"${execPath}" ${noOfHours} < "${inputFilePath}"`, (err) => {
+                //     if (err) {
+                //         console.log("err ", err)
+                //         reject({ err: err.message })
+                //     }
+                fs.readFile(outputFilePath, async (err, data) => {
+                    if (err)
                         reject({ err: err.message })
-                    }
-                    fs.readFile(outputFilePath, async (err, data) => {
-                        if (err)
-                            reject({ err: err.message })
-                        else {
-                            const outputFileData = JSON.parse(data.toString())
-                            const newOrderPromises = outputFileData.clusters.map(async (item: any) => {
-                                item.route?.shift()
-                                const endTime = item.route?.pop()?.time
-                                const rider = riders.find((rider: any) => rider.username === `dpartner_${item.riderId}`) as any
-                                const newCluster: Cluster = await prisma.cluster.create({
-                                    data: {
-                                        riderId: rider.id,
-                                        endTime: endTime
-                                    } as any,
-                                    include: {
-                                        order: true
-                                    }
-                                })
-                                return item.route?.map(async (order: any) => {
-                                    return new Promise(async (resolve, reject) => {
-                                        const newOrder: Order = await prisma.order.update({
-                                            where: {
-                                                awb: order.awb
-                                            } as any,
-                                            data: {
-                                                clusterId: newCluster.id,
-                                                reachTime: order.time,
-                                                status: "ASSIGNED"
-                                            }
-                                        })
-                                            .catch(err => { throw new Error("Error in updating orders") })
-                                        resolve(newOrder)
-                                    })
-                                })
-
+                    else {
+                        const outputFileData = JSON.parse(data.toString())
+                        const newOrderPromises = outputFileData.clusters.map(async (item: any) => {
+                            if (item === null) return Promise.resolve()
+                            item.route?.shift()
+                            const endTime = item.route?.pop()?.time
+                            const rider = riders.find((rider: any) => rider.username === `dpartner_${item.riderId}`) as any
+                            const newCluster: Cluster = await prisma.cluster.create({
+                                data: {
+                                    riderId: rider.id,
+                                    endTime: endTime
+                                } as any,
+                                include: {
+                                    order: true
+                                }
                             })
-                            Promise.all(newOrderPromises)
-                                .then(async () => {
-                                    resolve(await prisma.cluster.findMany({
-                                        include: {
-                                            order: {
-                                                include: {
-                                                    address: true
-                                                }
-                                            },
-                                            rider: true
+                            return item.route?.map(async (order: any) => {
+                                return new Promise(async (resolve, reject) => {
+                                    const newOrder: Order = await prisma.order.update({
+                                        where: {
+                                            awb: order.awb
+                                        } as any,
+                                        data: {
+                                            clusterId: newCluster.id,
+                                            reachTime: order.time,
+                                            status: "ASSIGNED"
                                         }
-                                    }))
+                                    })
+                                        .catch(err => { throw new Error("Error in updating orders") })
+                                    resolve(newOrder)
                                 })
-                        }
-                    })
+                            })
+
+                        })
+                        Promise.all(newOrderPromises)
+                            .then(async () => {
+                                resolve(await prisma.cluster.findMany({
+                                    include: {
+                                        order: {
+                                            include: {
+                                                address: true
+                                            }
+                                        },
+                                        rider: true
+                                    }
+                                }))
+                            })
+                    }
                 })
+                // })
             }
         })
     })
