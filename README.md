a5c759a0e93c0e5bc268dfae93e6371fc4acc5f3
Jayanth <ae20b030@smail.iitm.ac.in>
2023-02-08 18:12:40 +0530

    Updated readme, added dynamic point exe file and refactored

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 
+# The Backend
+
+The backend of the route planning algorithm application is a crucial component in ensuring efficient management of orders and riders. The backend serves as an intermediary between the web and app clients and the databases, performing tasks such as running the algorithms, processing requests, and returning data.
+
+## Setup Instructions
+
+Install Node JS and PG Admin for running the application.
+
+Run the following commands to install all the dependencies and initialize the database. 
+    
+```
+yarn install
+yarn db-generate
+yarn db-update
+```
+To build the application, run the command
+
+```
+yarn build
+```
+To start the server, run the command
+
+```
+yarn start
+```
+    
+## Environment Variables
+
+To run this project, you will need to add the following environment variables to your .env file. This file has to be placed inside the root directory. 
+
+`PORT=8000`
+
+`DB_URL=postgresql://<username>:<passowrd>@localhost:5432/interiit`
+
+`GOOGLE_MAP_API_KEY=<your google api key>`
+
+`JWT_SECRET=<your JWT secret token>` 
--- src/services/orders/addOrder.service.ts
+++ src/services/orders/addOrder.service.ts
@@ -5,6 +5,7 @@ import fs from "fs"
 import adminConfig from "../../admin/config"
 import { getGeocode } from "../../middleware/getGeocode"
 import { pickupInputSerializer } from "../../utils/pickupInputSerializer"
+import { exec } from "child_process"
 const prisma = new PrismaClient()
 
 export const addOrderService = async (address: string) => {
@@ -14,109 +15,154 @@ export const addOrderService = async (address: string) => {
     if (newGeoCodes instanceof Error)
         throw new Error("Error in fetching geocodes")
     else {
-        const riderIdfromAlgo = "1"
-        const rider: Rider | null = await prisma.rider.findUnique({
-            where: {
-                username: `dpartner_${riderIdfromAlgo}`
+        const [lat, lng]: [number, number] = newGeoCodes
+        const randomAWBnumber: number = Math.floor(Math.random() * 100000000000);
+        const randomSKU_ID: string = `SKU_${Math.floor(Math.random() * 1000)}`
+        const pickupInputFile = path.join(__dirname, "../../../src/algorithms/pickupInput.txt")
+        const pickupOutputFile = path.join(__dirname, "../../../src/algorithms/output.txt")
+        const execPath = path.join(__dirname, "../../../src/algorithms/dyn.out")
+        const newPickupPoint: Order = await prisma.order.create({
+            data: {
+                awb: `${randomAWBnumber}`,
+                productId: randomSKU_ID,
+                name: "warehouse",
+                status: "PENDING",
+                isDynamicPoint: true,
+                address: {
+                    create: {
+                        address: address,
+                        lat: lat,
+                        lng: lng,
+                    }
+                }
+            },
+            include: {
+                address: true
             }
         })
-        if (rider === null) {
-            throw new Error("Rider not found")
-        }
-        else {
-            const fcmToken = rider.fcmToken
-            const [lat, lng]: [number, number] = newGeoCodes
-            const randomAWBnumber: number = Math.floor(Math.random() * 100000000000);
-            const randomSKU_ID: string = `SKU_${Math.floor(Math.random() * 1000)}`
-            const pickupInputFile = path.join(__dirname, "../../../src/algorithms/pickupInput.txt")
-            const pickupOutputFile = path.join(__dirname, "../../../src/algorithms/pickupOutput.txt")
-            const newPickupPoint: Order = await prisma.order.create({
-                data: {
-                    awb: `${randomAWBnumber}`,
-                    productId: randomSKU_ID,
-                    name: "warehouse",
-                    status: "PENDING",
-                    isDynamicPoint: true,
-                    address: {
-                        create: {
-                            address: address,
-                            lat: lat,
-                            lng: lng,
-                        }
+        const clusters = await prisma.cluster.findMany({
+            where: {
+                order: {
+                    every: {
+                        status: "ASSIGNED"
                     }
-                },
-                include: {
-                    address: true
                 }
-            })
-            const clusters = await prisma.cluster.findMany({
-                where: {
-                    order: {
-                        every: {
-                            status: "ASSIGNED"
-                        }
+            },
+            select: {
+                rider: {
+                    select: {
+                        username: true
                     }
                 },
-                select: {
-                    rider: {
-                        select: {
-                            username: true
-                        }
-                    },
-                    order: {
-                        select: {
-                            awb: true,
-                            reachTime: true,
-                            address: {
-                                select: {
-                                    lat: true,
-                                    lng: true
-                                }
+                order: {
+                    select: {
+                        awb: true,
+                        reachTime: true,
+                        address: {
+                            select: {
+                                lat: true,
+                                lng: true
                             }
                         }
                     }
                 }
+            }
+        })
+            .then(clusters => clusters)
+            .catch(err => { throw new Error("Cannot add dynamic point") })
+        const [serializedClusters, noOfOrders] = pickupInputSerializer(clusters)
+        const noOfRiders = await prisma.rider.count() - 1   // subtracting admin
+        const noOfHours = 5
+        const warehouseLocation = {
+            lat: 12.971599,
+            lng: 77.638725
+        }
+        const pickupInput = {
+            noOfOrders: noOfOrders,
+            noOfRiders: noOfRiders,
+            warehouseLocation: warehouseLocation,
+            consignments: [newPickupPoint],
+            clusters: serializedClusters
+        }
+        const pickupResponse = new Promise(async (resolve, reject) => {
+            fs.writeFile(pickupInputFile, JSON.stringify(pickupInput), (err) => {
+                if (err) {
+                    reject({ err: err.message })
+                }
+                else {
+                    exec(`"${execPath}" ${noOfHours} < "${pickupInputFile}"`, (err) => {
+                        if (err) {
+                            reject({ err: err.message })
+                        }
+                        else {
+                            fs.readFile(pickupOutputFile, async (err, data) => {
+                                if (err) {
+                                    reject({ err: err.message })
+                                }
+                                else {
+                                    const outputFileData = JSON.parse(data.toString())
+                                    const riderId = outputFileData.cluster[0].riderId
+                                    const rider: Rider | null = await prisma.rider.findUnique({
+                                        where: {
+                                            username: `dpartner_${riderId}`
+                                        }
+                                    })
+                                    if (rider === null) {
+                                        throw new Error("Rider not found")
+                                    }
+                                    else {
+                                        const newReachTime = outputFileData.cluster[0].route.filter((route: any) => route.awb === randomAWBnumber).reachTime
+                                        const updatedOrder = await prisma.order.update({
+                                            where: {
+                                                awb: `${randomAWBnumber}`
+                                            },
+                                            data: {
+                                                status: "ASSIGNED",
+                                                reachTime: newReachTime,
+                                                rider: {
+                                                    connect: {
+                                                        username: `dpartner_${riderId}`
+                                                    }
+                                                }
+                                            } as any
+                                        })
+                                            .catch(err => { throw new Error("Cannot find rider to update") })
+                                        const fcmToken = rider.fcmToken
+                                        const message = {
+                                            notification: {
+                                                title: "New Pickup Point",
+                                                body: "You have a new pickup point at " + address
+                                            }
+                                        }
+
+                                        messaging(adminConfig).sendToDevice(fcmToken, message)
+                                            .then((response) => {
+                                                console.log("Successfully sent message:", response);
+                                            })
+                                            .catch((error) => {
+                                                console.log("Error sending message:", error);
+                                            })
+                                    }
+                                }
+                            })
+                        }
+                    })
+                }
+            })
+            resolve(await prisma.cluster.findMany({
+                include: {
+                    order: {
+                        include: {
+                            address: true
+                        }
+                    },
+                    rider: true
+                }
             })
                 .then(clusters => clusters)
                 .catch(err => { throw new Error("Cannot add dynamic point") })
-            const [serializedClusters, noOfOrders] = pickupInputSerializer(clusters)
-            const noOfRiders = await prisma.rider.count() - 1   // subtracting admin
-            const noOfHours = 5
-            const warehouseLocation = {
-                lat: 12.971599,
-                lng: 77.638725
-            }
-            const pickupInput = {
-                noOfOrders: noOfOrders,
-                noOfRiders: noOfRiders,
-                warehouseLocation: warehouseLocation,
-                consignments: [newPickupPoint],
-                clusters: serializedClusters
-            }
-            const pickupResponse = new Promise((resolve, reject) => {
-                fs.writeFile(pickupOutputFile, JSON.stringify(pickupInput), (err) => {
-                    if (err) {
-                        reject({ err: err.message })
-                    }
-                })
-                resolve("Done")
-            })
-
-            const message = {
-                notification: {
-                    title: "New Pickup Point",
-                    body: "You have a new pickup point at " + address
-                }
-            }
-
-            messaging(adminConfig).sendToDevice(fcmToken, message)
-                .then((response) => {
-                    console.log("Successfully sent message:", response);
-                })
-                .catch((error) => {
-                    console.log("Error sending message:", error);
-                })
-            return pickupResponse
-        }
+            )
+        })
+        return pickupResponse
     }
 }
